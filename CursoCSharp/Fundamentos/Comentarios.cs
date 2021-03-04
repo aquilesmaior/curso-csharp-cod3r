bf159add8bfac7b8b26b3696cf43671c6df6a36c
Aquiles Maior <aquilesmaior@gmail.com>
2021-03-04 11:30:49 -0300

    Projeto de Estudo Iniciado, com menu de Central de Exercícios

---
+++ CursoCSharp/Fundamentos/Comentarios.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoCSharp.Fundamentos {
+    class Comentarios {
+        public static void Executar() {
+            Console.WriteLine("TESTE COMENTARIOS");
+        }
+    }
+}
