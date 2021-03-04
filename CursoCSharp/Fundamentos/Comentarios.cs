f2f256b1765a7c9f3f79c5dad7841880c77bdb4b
Aquiles Maior <aquilesmaior@gmail.com>
2021-03-04 11:37:30 -0300

    Alterei só pra testar o git

--- CursoCSharp/Fundamentos/Comentarios.cs
+++ CursoCSharp/Fundamentos/Comentarios.cs
@@ -6,6 +6,7 @@ namespace CursoCSharp.Fundamentos {
     class Comentarios {
         public static void Executar() {
             Console.WriteLine("TESTE COMENTARIOS");
+            Console.Writeline("Teste 2 para git")
         }
     }
 }
